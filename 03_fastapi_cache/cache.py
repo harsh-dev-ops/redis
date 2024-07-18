b1f2b7f9411b5355a7e55b204ecefc2acb7575f9
Harsh Singh <harshkushwah2011@gmail.com>
2024-07-18 18:56:32 +0530

    fastapi cache with async redis

---
+++ 03_fastapi_cache/cache.py
@@ -0,0 +1,75 @@
+import datetime
+import time
+from typing import Optional
+import uvicorn
+
+import redis.asyncio as async_redis
+
+from fastapi import FastAPI, HTTPException
+from starlette.requests import Request
+from starlette.responses import Response
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from pydantic import EmailStr
+
+from redis_om.model import HashModel, NotFoundError
+from redis_om.connections import get_redis_connection
+
+# This Redis instance is tuned for durability.
+REDIS_DATA_URL = "redis://localhost:6379"
+
+# This Redis instance is tuned for cache performance.
+REDIS_CACHE_URL = "redis://localhost:6379"
+
+
+class Customer(HashModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    join_date: datetime.date
+    age: int
+    bio: Optional[str]
+
+    # You can set the Redis OM URL using the REDIS_OM_URL environment
+    # variable, or by manually creating the connection using your model's
+    # Meta object.
+    class Meta:
+        database = get_redis_connection(url=REDIS_DATA_URL, decode_responses=True)
+
+
+app = FastAPI()
+
+
+@app.post("/customer")
+async def save_customer(customer: Customer):
+    # We can save the model to Redis by calling `save()`:
+    return customer.save()
+
+
+@app.get("/customers")
+async def list_customers(request: Request, response: Response):
+    # To retrieve this customer with its primary key, we use `Customer.get()`:
+    return {"customers": Customer.all_pks()}
+
+
+@app.get("/customer/{pk}")
+@cache(expire=10)
+async def get_customer(pk: str, request: Request, response: Response):
+    # To retrieve this customer with its primary key, we use `Customer.get()`:
+    try:
+        time.sleep(2)
+        return Customer.get(pk)
+    except NotFoundError:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+
+@app.on_event("startup")
+async def startup():
+    r = async_redis.from_url(REDIS_CACHE_URL, encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(r), prefix="fastapi-cache")
+
+if __name__ == "__main__":
+    uvicorn.run("cache:app", reload=True, host="0.0.0.0", port=8001)
